a5ae4bd6c3b447dab8da5d2a0f41c91f90c62265
InatoInato <diasmuratbekff@gmail.com>
2025-06-12 20:04:11 +0500

    second commit

--- internal/user/model.go
+++ internal/user/model.go
@@ -9,3 +9,14 @@ type User struct {
 	Password string `json:"password"`
 	Role     string `json:"role"`
 }
+
+type RegisterDTO struct {
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
+
+type LoginDTO struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
--- internal/user/service.go
+++ internal/user/service.go
@@ -1,1 +1,34 @@
 package user
+
+import "server/pkg/utils"
+
+type Service interface {
+	Register(input RegisterDTO) error
+	Login(input LoginDTO) (string, error)
+}
+
+type service struct {
+	repo Repository
+}
+
+func NewService(r Repository) Service {
+	return &service{repo: r}
+}
+
+func (s *service) Register(input RegisterDTO) error {
+	hashed, err := utils.HashPassword(input.Password)
+
+	if err != nil {
+		return err
+	}
+
+	user := &User{
+		Email: input.Email,
+		Name: input.Name,
+		Email: input.Email,
+	}
+}
+
+func Login() {
+
+}
